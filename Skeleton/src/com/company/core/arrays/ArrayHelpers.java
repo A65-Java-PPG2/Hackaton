844c87911130aa46979faa3c895572179f3989dd
Nikolay Voynov <nikolay.voynov.a65@learn.telerikacademy.com>
2024-07-16 17:16:22 +0300

    methods divided between Niki, Emo & Iso

--- Skeleton/src/com/company/core/arrays/ArrayHelpers.java
+++ Skeleton/src/com/company/core/arrays/ArrayHelpers.java
@@ -1,4 +1,4 @@
-package com.company.core.arrays;
+package Skeleton.src.com.company.core.arrays;
 
 @SuppressWarnings({"ManualArrayCopy", "ExplicitArrayFilling"})
 public class ArrayHelpers {
@@ -9,56 +9,70 @@ public class ArrayHelpers {
         return new int[1];
     }
 
+    // Nikolay Voynov
+
     public static int[] addFirst(int[] source, int element) {
         return new int[1];
     }
+    // Nikolay Voynov
 
     public static int[] addAll(int[] source, int... elements) {
         return new int[1];
     }
+    // Nikolay Voynov
 
     public static boolean contains(int[] source, int element) {
         return false;
     }
+    // Nikolay Voynov
 
     public static void copy(int[] sourceArray, int[] destinationArray, int count) {
 
     }
+    // Nikolay Voynov
 
     public static void copyFrom(int[] sourceArray, int sourceStartIndex,
                                 int[] destinationArray, int destStartIndex, int count) {
 
     }
+    // Emiliyan Geshkov
 
     public static void fill(int[] source, int element) {
 
     }
+    // Emiliyan Geshkov
 
     public static int firstIndexOf(int[] source, int target) {
         return 0;
     }
+    // Emiliyan Geshkov
 
     public static int[] insert(int[] source, int index, int element) {
         return new int[1];
     }
+    // Emiliyan Geshkov
 
     public static boolean isValidIndex(int[] source, int index) {
         return false;
     }
+    // Emiliyan Geshkov
 
     public static int lastIndexOf(int[] source, int target) {
         return 0;
     }
+    // Ismail Emin
 
     public static int[] removeAllOccurrences(int[] source, int element) {
         return new int[1];
     }
-
+    // Ismail Emin
     public static void reverse(int[] arrayToReverse) {
     }
+    // Ismail Emin
 
     public static int[] section(int[] source, int startIndex, int endIndex) {
         return new int[1];
     }
+    // Ismail Emin
 
 }
--- Skeleton/src/com/company/core/strings/StringHelpers.java
+++ Skeleton/src/com/company/core/strings/StringHelpers.java
@@ -1,4 +1,4 @@
-package com.company.core.strings;
+package Skeleton.src.com.company.core.strings;
 
 @SuppressWarnings("StringConcatenationInLoop")
 public class StringHelpers {
@@ -6,59 +6,73 @@ public class StringHelpers {
     public static String abbreviate(String source, int maxLength) {
         return null;
     }
+    // Nikolay Voynov
 
     public static String capitalize(String source) {
         return null;
     }
+    // Nikolay Voynov
 
     public static String concat(String string1, String string2) {
         return null;
     }
+    // Nikolay Voynov
 
     public static boolean contains(String source, char symbol) {
         return false;
     }
+    // Nikolay Voynov
 
     public static boolean endsWith(String source, char target) {
         return false;
     }
+    // Nikolay Voynov
 
     public static int firstIndexOf(String source, char target) {
         return 0;
     }
+    // Emiliyan Geshkov
 
     public static int lastIndexOf(String source, char symbol) {
         return 0;
     }
+    // Emiliyan Geshkov
 
     public static String pad(String source, int length, char paddingSymbol) {
         return null;
 
     }
+    // Emiliyan Geshkov
 
     public static String padEnd(String source, int length, char paddingSymbol) {
         return null;
     }
+    // Emiliyan Geshkov
 
     public static String padStart(String source, int length, char paddingSymbol) {
         return null;
 
     }
+    // Emiliyan Geshkov
 
     public static String repeat(String source, int times) {
         return null;
     }
+    // Ismail Emin
 
     public static String reverse(String source) {
         return null;
     }
+    // Ismail Emin
 
     public static String section(String source, int start, int end) {
         return null;
     }
+    // Ismail Emin
 
     public static boolean startsWith(String source, char target) {
         return false;
     }
+    // Ismail Emin
 
 }
